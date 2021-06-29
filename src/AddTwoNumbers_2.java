cf44919691edcc9a9f222b0beb7625f883ee161b
黄泽彬 <1763152010@qq.com>
2021-06-29 22:56:23 +0800

    feat: finish question 2

---
+++ src/AddTwoNumbers_2.java
@@ -0,0 +1,45 @@
+/**
+ * 链表
+ */
+public class AddTwoNumbers_2 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode pre = new ListNode(-1);
+        ListNode cur = pre;
+        int temp = 0;
+        while (l1 != null || l2 != null) {
+            int a = l1 == null ? 0 : l1.val;
+            int b = l2 == null ? 0 : l2.val;
+            int sum = a + b + temp;
+            temp = sum / 10;
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (temp == 1) {
+            cur.next = new ListNode(1);
+        }
+        return pre.next;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
